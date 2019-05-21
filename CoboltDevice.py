7bdab7c07e53788d024f888b71c0965286b007bc
Daniel Cote <dccote@novajo.ca>
2019-05-21 12:21:00 -0400

    Update CoboltDevice.py

--- CoboltDevice.py
+++ CoboltDevice.py
@@ -1,6 +1,7 @@
 from PhysicalDevice import *
 from CommunicationPort import *
 import numpy as np
+from typing import Protocol
 
 class LaserSourceDevice:
     def __init__():
@@ -23,15 +24,22 @@ class CoboltDevice(PhysicalDevice, LaserSourceDevice):
 
     def __init__(bsdPath=None, serialNumber: str = None,
                  productId: np.uint32 = None, vendorId: np.uint32 = None):
-
+        self.laserSerialNumber = None
         self.port = CommunicationPort(bsdPath=bsdPath)
         PhysicalDevice.__init__(vendorId, productId, serialNumber)
         LaserSourceDevice.__init__()
 
     def doInitializeDevice():
-        self.port.open()
-        return
+        try:
+            self.port.open()
+            self.doGetLaserSerialNumber()
+        except error:
+            self.port.close()
+            raise error
 
     def doShutdownDevice():
         self.port.close()
         return
+
+    def doGetLaserSerialNumber():
+        self.laserSerialNumber = self.port.writeStringReadFirstMatchingGroup("sn?\r", replyPattern="(\\d+)")
