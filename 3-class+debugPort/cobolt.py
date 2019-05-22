14e1a05e208bb04e4d855c35bea41e26102a26fe
Daniel Cote <dccote@novajo.ca>
2019-05-22 11:27:10 -0400

    Created a debug port for cobolt.py, alllowed correction for CoboltLaser class

---
+++ 3-class+debugPort/cobolt.py
@@ -0,0 +1,79 @@
+try:
+    import serial as SP
+except:
+    exit("pyserial must be installed with: pip install pyserial")
+import re
+
+class CoboltDebugSerial:
+    def __init__(self):
+        self.outputBuffer = bytearray()
+        self.lineEnding = b'\n'
+        self.power = 0.1
+
+    def open(self):
+        return
+
+    def close(self):
+        return
+
+    def write(self, data:bytearray) -> int :
+        string = data.decode('utf-8')
+
+        match = re.search("pa\\?", string)
+        if match is not None:
+            replyData = bytearray("{0}\r".format(self.power), encoding='utf-8')
+            self.outputBuffer.extend(replyData)
+            return len(data)
+
+        match = re.search("p (\\d+\\.?\\d+)\r", string)
+        if match is not None:
+            self.power = match.groups()[0]
+            return len(data)
+
+        raise ValueError("Unknown command: {0}".format(data))
+
+    def readline(self) -> bytearray:
+        data = bytearray()
+        for c in self.outputBuffer:
+            data.append(c)
+            if c == self.lineEnding:
+                break
+        return data
+
+
+""" Class-oriented strategy to talk to the Cobolt
+laser, change its power. See manual in ../manuals/ page 27"""
+
+class CoboltLaser:
+
+    def __init__(self, portName):
+        """ portName : "COM1", "/dev/xxx", etc... 
+        opens automatically on creation """
+        self.port = None
+        if portName == "debug":
+            self.port = CoboltDebugSerial()
+        else:
+            self.port = SP.Serial(portName)
+
+    def __del__(self):
+        if self.port is not None:
+            self.port.close()
+
+    def setPower(self, powerInWatts):
+        command = "p {0:0.3f}\r".format(powerInWatts)
+        data = bytearray(command, "utf-8")
+        self.port.write(data)
+
+    def power(self):    
+        self.port.write(b'pa?\r') 
+        reply = self.port.readline()
+        print(reply)
+        power = float(reply.decode())
+        return power
+
+
+if __name__ == "__main__":
+    laser = CoboltLaser("debug")
+    laser.setPower(powerInWatts=0.1)
+    print(laser.power())
+    
