9e692ea60eddd4d0b6c51b88fa82c18b7600eacf
Daniel <dccote@cervo.ulaval.ca>
2021-03-19 13:05:37 -0400

    Split CommPort and SerrialPort

--- hardwarelibrary/communicationport.py
+++ hardwarelibrary/communicationport.py
@@ -18,67 +18,33 @@ class CommunicationPort:
     functions to write strings and read strings, and abstract away
     the details of the communication.
 
-    Two strategies to initialize the CommunicationPort:
-    1. with a bsdPath/port name (i.e. "COM1" or "/dev/cu.serial")
-    2. with an instance of SerialPort() that will support the same
-       functions as pyserial.Serial() (open, close, read, write, readline)
     """
     
-    def __init__(self, bsdPath=None, portPath=None, port = None):
-        if bsdPath is not None:
-            self.portPath = bsdPath
-        elif portPath is not None:
-            self.portPath = portPath
-        else:
-            self.portPath = None
-
-        if port is not None and port.is_open:
-            port.close()
-        self.port = port # direct port, must be closed.
-
+    def __init__(self):
         self.portLock = RLock()
         self.transactionLock = RLock()
 
     @property
     def isOpen(self):
-        if self.port is None:
-            return False    
-        else:
-            return self.port.is_open    
+        raise NotImplementedError()
 
     def open(self):
-        if self.port is None:
-            self.port = serial.Serial(self.portPath, 19200, timeout=0.3)
-        else:
-            self.port.open()
+        raise NotImplementedError()
 
     def close(self):
-        self.port.close()
+        raise NotImplementedError()
 
     def bytesAvailable(self) -> int:
-        return self.port.in_waiting
+        raise NotImplementedError()
 
     def flush(self):
-        if self.isOpen:
-            self.port.reset_input_buffer()
-            self.port.reset_output_buffer()
+        raise NotImplementedError()
 
     def readData(self, length, endpoint=0) -> bytearray:
-        with self.portLock:
-            data = self.port.read(length)
-            if len(data) != length:
-                raise CommunicationReadTimeout()
-
-        return data
+        raise NotImplementedError()
 
     def writeData(self, data, endpoint=0) -> int:
-        with self.portLock:
-            nBytesWritten = self.port.write(data)
-            if nBytesWritten != len(data):
-                raise IOError("Not all bytes written to port")
-            self.port.flush()
-
-        return nBytesWritten
+        raise NotImplementedError()
 
     def readString(self, endpoint=0) -> str:      
         with self.portLock:
@@ -183,3 +149,69 @@ class DebugEchoCommunicationPort(CommunicationPort):
             self.buffer.extend(data)
 
         return len(data)
+
+class SerialPort(CommunicationPort):
+    """
+    An implementation of CommunicationPort using BSD-style serial port
+    
+    Two strategies to initialize the SerialPort:
+    1. with a bsdPath/port name (i.e. "COM1" or "/dev/cu.serial")
+    2. with an instance of pyserial.Serial() that will support the same
+       functions as pyserial.Serial() (open, close, read, write, readline)
+    """
+    def __init__(self, bsdPath=None, portPath=None, port = None):
+        CommunicationPort.__init__(self, )
+        if bsdPath is not None:
+            self.portPath = bsdPath
+        elif portPath is not None:
+            self.portPath = portPath
+        else:
+            self.portPath = None
+
+        if port is not None and port.is_open:
+            port.close()
+        self.port = port # direct port, must be closed.
+
+        self.portLock = RLock()
+        self.transactionLock = RLock()
+
+    @property
+    def isOpen(self):
+        if self.port is None:
+            return False    
+        else:
+            return self.port.is_open    
+
+    def open(self):
+        if self.port is None:
+            self.port = serial.Serial(self.portPath, 19200, timeout=0.3)
+        else:
+            self.port.open()
+
+    def close(self):
+        self.port.close()
+
+    def bytesAvailable(self) -> int:
+        return self.port.in_waiting
+
+    def flush(self):
+        if self.isOpen:
+            self.port.reset_input_buffer()
+            self.port.reset_output_buffer()
+
+    def readData(self, length, endpoint=0) -> bytearray:
+        with self.portLock:
+            data = self.port.read(length)
+            if len(data) != length:
+                raise CommunicationReadTimeout()
+
+        return data
+
+    def writeData(self, data, endpoint=0) -> int:
+        with self.portLock:
+            nBytesWritten = self.port.write(data)
+            if nBytesWritten != len(data):
+                raise IOError("Not all bytes written to port")
+            self.port.flush()
+
+        return nBytesWritten
