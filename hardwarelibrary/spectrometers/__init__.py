1a27925029be9ff0bea7e6632f44a352a47b4298
Daniel <dccote@cervo.ulaval.ca>
2021-04-12 23:50:27 -0400

    Functions return PyUSB devices, not real devices, added back USB to name

--- hardwarelibrary/spectrometers/__init__.py
+++ hardwarelibrary/spectrometers/__init__.py
@@ -5,8 +5,8 @@ from .oceaninsight import OISpectrometer, USB2000, USB4000, SpectraViewer
 def any() -> OISpectrometer:
     return OISpectrometer.any()
 
-def connectedDevices(idProduct=None, serialNumber=None):
+def connectedUSBDevices(idProduct=None, serialNumber=None):
     return OISpectrometer.connectedUSBDevices(idProduct=idProduct, serialNumber=serialNumber)
 
-def matchUniqueDevice(idProduct=None, serialNumber=None):
+def matchUniqueUSBDevice(idProduct=None, serialNumber=None):
     return OISpectrometer.matchUniqueUSBDevice(idProduct=idProduct, serialNumber=serialNumber)
