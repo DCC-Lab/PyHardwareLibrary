0bbebbac6753d29049d467092be550c6f9b27916
Daniel <dccote@cervo.ulaval.ca>
2021-04-23 09:15:00 -0400

    Tests pass, including the buggy command work-around

--- hardwarelibrary/tests/testsIntegra.py
+++ hardwarelibrary/tests/testsIntegra.py
@@ -24,13 +24,6 @@ class TestIntegraPort(unittest.TestCase):
         self.assertIsNotNone(reply)
         self.assertTrue(float(reply) != 0)
 
-    # def testReadPowerForever(self):
-    #     while True:
-    #         self.port.writeData(data=b"*CVU")
-    #         reply = self.port.readString()
-    #         self.assertIsNotNone(reply)
-    #         print("Power is {0:0.1f}".format(float(reply)*1000))
-
     def testValidateCommands(self):
         commands = [b'*VEr',b'*STS', b'ST2', b'*CVU',b'*GWL',b'*GAN',b'*GZO',b'*GAS',b'*GCR',b'SSU',b'SSD',b'*DVS']
         for command in commands:
@@ -39,13 +32,11 @@ class TestIntegraPort(unittest.TestCase):
                 if command != b'*DVS':
                     reply = self.port.readString()
                     self.assertIsNotNone(reply)
-                    # print("{0} replied {1}".format(command, reply))
                 else:
                     while True:
                         try:
                             reply = self.port.readString()
                             self.assertIsNotNone(reply)
-                            # print("{0} replied {1}".format(command, reply))
                         except:
                             break
             except:
@@ -61,7 +52,6 @@ class TestIntegraPort(unittest.TestCase):
 
         for command in commands:
             self.assertFalse(command.send(port=self.port),command.exceptions)
-            print(command.matchGroups[0])
 
             with self.assertRaises(Exception):
                 leftover = self.port.readString()
@@ -69,22 +59,21 @@ class TestIntegraPort(unittest.TestCase):
 
 
     def testStatusCommand(self):
-        TextCommand(name="STATUS", text="*STS", replyPattern=r"(.+)\r\n", finalReplyPattern=":100000000"),
-
+        #TextCommand(name="STATUS", text="*STS", replyPattern=r"(.+)\r\n", finalReplyPattern=":100000000"),
         self.port.writeString("*STS")
-        try:
-            while True:
+
+        for i in range(47):
+            try:
                 reply = self.port.readString()
-                if reply == ':100000000':
-                    break
-        except:
-            print("Done")
+            except:
+                print("Failed at i {0}".format(i))
 
     def testIntegraBugUnresponsiveAfterSetWavelength(self):
-        """ While testing, I found that the setwavelength command requires
-        a sleep after (it does not confirm the command with anything).
-        If we don't sleep, the GETWAVELENGTH will return nothing.
-        My experience is that the delay can be as low as 0.023 and as high as 0.03
+        """ While testing an Integra new version with a UP19K-15S-H5-INT-D0, 
+        I found that the setwavelength command requires
+        a sleep after being sent (it does not confirm the command with anything).
+        If we don't sleep, the next command will return nothing.
+        My experience is that the required delay can be as low as 0.023 and as high as 0.03
 
         It should always succeed for >0.05 and should always fail with ≤ 0.02
         It is not sufficient to set a longer timeout as demonstrated with
@@ -105,12 +94,7 @@ class TestIntegraPort(unittest.TestCase):
         self.assertTrue(getCommand.send(port=self.port),msg="Surprisingly succeeded sending command with delay 0.001")
 
     def testIntegraBugUnresponsiveAfterSetWavelengthMustSleep(self):
-        """ While testing, I found that the setwavelength command requires
-        a sleep after (it does not confirm the command with anything).
-        If we don't sleep, the GETWAVELENGTH will return nothing.
-        My experience is that the delay can be as low as 0.023 and as high as 0.03
-
-        It is not sufficient to set a longer timeout, as is demonstrated here
+        """ See above.
         """
         setCommand = TextCommand(name="SETWAVELENGTH", text="*PWC{0:05d}")
         getCommand = TextCommand(name="GETWAVELENGTH", text="*GWL", replyPattern = r"PWC\s*:\s*(.+?)\r\n")
@@ -120,6 +104,16 @@ class TestIntegraPort(unittest.TestCase):
         # We except an error! setting a long timeout is not enough
         self.assertTrue(getCommand.send(port=self.port))
         self.assertTrue(isinstance(getCommand.exceptions[0], OSError))
+
+    def testIntegraBugUnresponsiveOnlyAfterSetWavelength(self):
+        """ See above.
+        """
+        setCommand = TextCommand(name="SETAUTOSCALE", text="*SAS1")
+        getCommand = TextCommand(name="GETWAVELENGTH", text="*GWL", replyPattern = r"PWC\s*:\s*(.+?)\r\n")
+        # This will succeed, so it's only Setwavelength that is the problem
+        setCommand.send(port=self.port, params=(800))
+        time.sleep(0.001)
+        self.assertFalse(getCommand.send(port=self.port),msg="Surprisingly succeeded sending command with delay 0.001")
 
 if __name__ == '__main__':
     unittest.main()
