0eaa77bff3a29ff496cfab774cdeb68fcafd7d0a
Justine Major <jumaj7@ulaval.ca>
2021-05-19 14:46:55 -0400

    test de base Sutter pour déplacer et lire la position
    Plusieurs petits bug avec le Sutter sur Catalina 10.12

---
+++ hardwarelibrary/tests/testSutterDevice.py
@@ -0,0 +1,71 @@
+import env # modifies path
+import unittest
+import time
+from threading import Thread, Lock
+import random
+import array
+import os
+from struct import *
+
+from hardwarelibrary.communication import *
+import serial
+
+"""
+class TestSerialPortFromDaniel(unittest.TestCase):
+    def testInit(self):
+        self.assertTrue(True)
+
+    def testPortFound(self):
+        port = SerialPort(idVendor=4930, idProduct=1)
+        self.assertIsNotNone(port)
+
+    def testPortOpen(self):
+        port = SerialPort(idVendor=4930, idProduct=1)
+        self.assertIsNotNone(port)
+        port.open()
+"""
+
+
+class TestPySerial(unittest.TestCase):
+
+    def testPySerialPortNotNoneWhenEmpty(self):
+        port = serial.Serial()
+        self.assertIsNotNone(port)
+
+    def testPySerialPortCannotOpenWhenEmpty(self):
+        port = serial.Serial()
+        self.assertIsNotNone(port)
+        with self.assertRaises(Exception):
+            port.open()
+
+    def testPySerialPortSutter(self):
+        port = serial.Serial("/dev/cu.usbserial-SI8YCLBE")
+        self.assertIsNotNone(port)
+        with self.assertRaises(Exception):
+            # ALready open will raise exception
+            port.open()
+        port.close()
+
+
+    def testPySerialPortSutterReadCommand(self):
+        x,y,z  = 0,0,0
+        port = serial.Serial("/dev/cu.usbserial-SI8YCLBE", timeout=5, baudrate=128000)
+        commandBytes = pack('<clllc', b'M', x, y, z, b'\r')
+        nBytes = port.write(commandBytes)
+        self.assertTrue(nBytes == len(commandBytes))
+        replyBytes = port.read(1)
+        self.assertTrue(replyBytes == b"\r")
+        port.close()
+
+    def testSutterReadPosition(self):
+        port = serial.Serial("/dev/cu.usbserial-SI8YCLBE", timeout=5, baudrate=128000)
+        commandBytes = pack('<cc', b'C', b'\r')
+        nBytes = port.write(commandBytes)
+        self.assertTrue(nBytes == len(commandBytes))
+        replyBytes = port.read(13)
+        print(replyBytes)
+        self.assertTrue(len(replyBytes) == 13)
+        port.close()
+
+if __name__ == '__main__':
+    unittest.main()
