73a87be141726a61ea73284491c6227567730766
Daniel <dccote@cervo.ulaval.ca>
2021-05-27 00:59:29 -0400

    Moved port to a  variable to be able to close it on tearDown

--- hardwarelibrary/tests/testSutterDevice.py
+++ hardwarelibrary/tests/testSutterDevice.py
@@ -9,54 +9,63 @@ import serial
 
 class TestPySerial(unittest.TestCase):
     def setUp(self):
+        self.port = None
         if os.name == 'nt':
             self.portPath = "COM4"
-        else:
+        else: # macOS
             self.portPath = "/dev/cu.usbserial-SI8YCLBE"
 
+    def tearDown(self):
+        self.port.close()
+
     def testPySerialPortNotNoneWhenEmpty(self):
-        port = serial.Serial()
-        self.assertIsNotNone(port)
+        self.port = serial.Serial()
+        self.assertIsNotNone(self.port)
 
     def testPySerialPortCannotOpenWhenEmpty(self):
-        port = serial.Serial()
-        self.assertIsNotNone(port)
+        self.port = serial.Serial()
+        self.assertIsNotNone(self.port)
         with self.assertRaises(Exception):
-            port.open()
+            self.port.open()
 
     def testPySerialPortSutter(self):
-        port = serial.Serial(self.portPath)
-        self.assertIsNotNone(port)
+        self.port = serial.Serial(self.portPath)
+        self.assertIsNotNone(self.port)
         with self.assertRaises(Exception):
             # ALready open will raise exception
-            port.open()
-        port.close()
+            self.port.open()
+        self.port.close()
 
+    def testPortClosesProperly(self):
+        self.port = serial.Serial(self.portPath, timeout=5, baudrate=128000)
+        self.port.close()
+        self.assertFalse(self.port.is_open)
+        
     def testPySerialPortSutterReadCommand(self):
         x,y,z  = 100000,0,0 
-        port = serial.Serial(self.portPath, timeout=5, baudrate=128000)
+        self.port = serial.Serial(self.portPath, timeout=5, baudrate=128000)
         commandBytes = pack('<clllc', b'M', x, y, z, b'\r')
-        nBytes = port.write(commandBytes)
+        nBytes = self.port.write(commandBytes)
         self.assertTrue(nBytes == len(commandBytes))
-        replyBytes = port.read(1)
+        replyBytes = self.port.read(1)
         print(replyBytes)
         self.assertTrue(replyBytes == b"\r")
-        port.close()
+        self.port.close()
 
     def testSutterReadPosition(self):
-        port = serial.Serial(self.portPath, timeout=5, baudrate=128000)
+        self.port = serial.Serial(self.portPath, timeout=5, baudrate=128000)
+        
         commandBytes = pack('<cc', b'C', b'\r')
-        nBytes = port.write(commandBytes)
+        nBytes = self.port.write(commandBytes)
         self.assertTrue(nBytes == len(commandBytes))
-        replyBytes = port.read(13)
+
+        replyBytes = self.port.read(13)
         print(replyBytes)
         info = unpack("<clll", replyBytes)
         print(info)
         print(info[1:])
         print(info[3]/16)
         self.assertTrue(len(replyBytes) == 13)
-        port.close()
-        self.assertFalse(port.is_open)
 
 
 if __name__ == '__main__':
