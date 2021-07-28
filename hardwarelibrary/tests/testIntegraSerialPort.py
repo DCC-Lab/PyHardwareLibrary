f47be5884512a494333740bff0958c0c7dd1492c
Daniel <dccote@cervo.ulaval.ca>
2021-07-28 17:46:12 -0400

    Update testIntegraSerialPort.py

--- hardwarelibrary/tests/testIntegraSerialPort.py
+++ hardwarelibrary/tests/testIntegraSerialPort.py
@@ -1,22 +1,36 @@
 # This script is called firstIntegraCommunication.py
+import env # modifies path
+import unittest
 import usb.core
 import usb.util
 from array import array 
 
-device = usb.core.find(idVendor=0x1ad5, idProduct=0x0300) # Find our device
-if device is None:
-    raise IOError("Can't find device")
+from hardwarelibrary.communication import USBPort, TextCommand
 
-device.set_configuration()                                     # First (and only) configuration
-configuration = device.get_active_configuration()              # Confirm configuration
-interface = configuration[(0,0)]                               # Get the first interface, no alternate
+class TestIntegraPort(unittest.TestCase):
+    def setUp(self):
+        device = usb.core.find(idVendor=0x1ad5, idProduct=0x0300) # Find our device
+        if device is None:
+            raise (unittest.SkipTest("No Integra devices connected"))
 
-interruptEndpoint = interface[0]                               # Not useful
-outputEndpoint = interface[1]                                  # Our output bulk OUT
-inputEndpoint = interface[2]                                   # Our input bulk IN
+    def testFirstCommunication(self):
+        device = usb.core.find(idVendor=0x1ad5, idProduct=0x0300) # Find our device
+        if device is None:
+            raise IOError("Can't find device")
 
-outputEndpoint.write("*VER")                                   # The command, no '\r' or '\n'
+        device.set_configuration()                                     # First (and only) configuration
+        configuration = device.get_active_configuration()              # Confirm configuration
+        interface = configuration[(0,0)]                               # Get the first interface, no alternate
 
-buffer = array('B',[0]*inputEndpoint.wMaxPacketSize)           # Buffer with maximum size
-bytesRead = inputEndpoint.read(size_or_buffer=buffer)          # Read and print as a string
-print( bytearray(buffer[:bytesRead]).decode(encoding='utf-8')) # Buffer is not resized, we do it ourselves
+        interruptEndpoint = interface[0]                               # Not useful
+        outputEndpoint = interface[1]                                  # Our output bulk OUT
+        inputEndpoint = interface[2]                                   # Our input bulk IN
+
+        outputEndpoint.write("*VER")                                   # The command, no '\r' or '\n'
+
+        buffer = array('B',[0]*inputEndpoint.wMaxPacketSize)           # Buffer with maximum size
+        bytesRead = inputEndpoint.read(size_or_buffer=buffer)          # Read and print as a string
+        print( bytearray(buffer[:bytesRead]).decode(encoding='utf-8')) # Buffer is not resized, we do it ourselves
+
+if __name__ == '__main__':
+    unittest.main()
