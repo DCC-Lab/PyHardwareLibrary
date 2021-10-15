43f7ac882b998064cfdf68842dc14a20680781af
Daniel <dccote@cervo.ulaval.ca>
2021-10-15 15:22:05 -0400

    MOre control requests.  I am starting to understand.

--- hardwarelibrary/tests/testUVCCamera.py
+++ hardwarelibrary/tests/testUVCCamera.py
@@ -3,6 +3,48 @@ import unittest
 import enum
 import usb.core
 import usb.util
+import struct
+from typing import NamedTuple
+
+class DeviceDescriptor(NamedTuple):
+    bLength: int
+    bDescriptorType: int
+    bcdUSB : int
+    bDeviceClass : int
+    bDeviceSubClass : int
+    bDeviceProtocol : int
+    bMaxPacketSize0 : int
+    idVendor : int
+    idproduct : int
+    bcdDevice : int
+    iManufacturer : int
+    iProduct : int
+    iSerialNumber : int
+    bNumConfigurations : int
+    packingFormat = "<BBHBBBBHHHBBBB"
+
+class ConfigurationDescriptor(NamedTuple):
+    bLength: int
+    bDescriptorType: int
+    wTotalLength : int
+    bNumInterfaces : int
+    bConfigurationValue : int
+    iConfiguration : int
+    bmAttributes : int
+    bMaxPower : int
+    packingFormat = "<BBHBBBBB"
+
+class InterfaceDescriptor(NamedTuple):
+    bLength: int
+    bDescriptorType: int
+    bInterfaceNumber : int
+    bAlternateSetting : int
+    bNumEndpoints : int
+    bInterfaceClass : int
+    bInterfaceSubClass : int
+    bInterfaceProtocol : int
+    iInterface : int
+    packingFormat = "<BBBBBBBBB"
 
 # """
 # USB information available at https://www.beyondlogic.org/usbnutshell/usb6.shtml
@@ -91,7 +133,10 @@ class TestUVCCamera(unittest.TestCase):
                       wValue=(usb.util.DESC_TYPE_DEVICE << 8) ,
                       wIndex=0,
                       data_or_wLength=0x12)
-        print(ret)
+
+        theStruct = struct.unpack(DeviceDescriptor.packingFormat, ret)
+        desc = DeviceDescriptor(*theStruct)
+        print(desc)
 
     def testGetConfigurationDescriptor(self):
         device = usb.core.find(idVendor=0x05ac, idProduct=0x1112)
@@ -104,8 +149,12 @@ class TestUVCCamera(unittest.TestCase):
         # print(ret)
         self.assertTrue(len(ret) >= 2)
 
+        theStruct = struct.unpack(ConfigurationDescriptor.packingFormat, ret)
+        desc = ConfigurationDescriptor(*theStruct)
+
         wTotalLength = ret[2] | (ret[3]<<8)
-        print(wTotalLength)
+        self.assertEqual(desc.wTotalLength, wTotalLength)
+
         ret = device.ctrl_transfer(usb.util.CTRL_IN | usb.util.CTRL_TYPE_STANDARD | usb.util.CTRL_RECIPIENT_DEVICE,
                       bRequest=Request.GET_DESCRIPTOR,
                       wValue=(usb.util.DESC_TYPE_CONFIG << 8),
@@ -113,5 +162,27 @@ class TestUVCCamera(unittest.TestCase):
                       data_or_wLength=wTotalLength)
         print(ret)
 
+    @unittest.skip("testGetInterfaceDescriptor: This is not possible: we must use the full configuration request")
+    def testGetInterfaceDescriptor(self):
+        """
+        Set Descriptor/Get Descriptor is used to return the specified descriptor in wValue. A request for the configuration descriptor will return the device descriptor and all interface and endpoint descriptors in the one request.
+
+        Endpoint Descriptors cannot be accessed directly by a GetDescriptor/SetDescriptor Request.
+        Interface Descriptors cannot be accessed directly by a GetDescriptor/SetDescriptor Request.
+        String Descriptors include a Language ID in wIndex to allow for multiple language support.
+        """
+        device = usb.core.find(idVendor=0x05ac, idProduct=0x1112)
+
+        ret = device.ctrl_transfer(usb.util.CTRL_IN | usb.util.CTRL_TYPE_STANDARD | usb.util.CTRL_RECIPIENT_DEVICE,
+                      bRequest=Request.GET_DESCRIPTOR,
+                      wValue=(usb.util.DESC_TYPE_INTERFACE << 8),
+                      wIndex=0,
+                      data_or_wLength=0x09)
+
+        self.assertTrue(len(ret) >= 2)
+        print(ret)
+        theStruct = struct.unpack(InterfaceDescriptor.packingFormat, ret)
+        desc = InterfaceDescriptor(*theStruct)
+        print(desc)
 if __name__ == '__main__':
     unittest.main()
