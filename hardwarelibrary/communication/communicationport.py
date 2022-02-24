df5530b8d388c4763beb439ae57e10942282ee45
Daniel <dccote@cervo.ulaval.ca>
2022-02-23 23:35:11 -0500

    Added ctsrts and dtr options when opening
    Hardware flow control

--- hardwarelibrary/communication/communicationport.py
+++ hardwarelibrary/communication/communicationport.py
@@ -63,11 +63,14 @@ class CommunicationPort:
         with self.portLock:
             byte = None
             data = bytearray(0)
-            while (byte != b''):
-                byte = self.readData(1, endPoint)
-                data += byte
-                if byte == b'\n':
-                    break
+            try:
+                while (byte != b''):
+                    byte = self.readData(1, endPoint)
+                    data += byte
+                    if byte == b'\n':
+                        break
+            except CommunicationReadTimeout as err:
+                raise CommunicationReadTimeout("Only obtained {0}".format(data))
 
             string = data.decode(encoding='utf-8')
 
